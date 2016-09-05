8ef001e500d90b763ef41d426c0b686067346b10
Jason Norwood-Young <jason@10layer.com>
2016-09-05 15:02:57 +0200

    Initial checkin

---
+++ README.md
@@ -0,0 +1,8 @@
+= A happy password generator =
+
+Uses positive words to generate a memorable password, in the format "WordWord999", where 999 is a number between 10 and 999. The words are selected from the list at https://github.com/jeffreybreen/twitter-sentiment-analysis-tutorial-201107/blob/master/data/opinion-lexicon-English/positive-words.txt, published by Jeffrey Breen under an Apache license. (For brevity, I've included just the file, which has more info in the header.)
+
+*NB NB This password generator does not generate secure passwords.* It's job is to produce positive, readable, memorable passwords, and its entropy is much too low for any serious security application. We use it to protect very low-value (free, actually) services where no personal information can be gleaned. 
+
+For a similar, more secure option, try http://preshing.com/20110811/xkcd-password-generator/ or one of the memorable password packages on NPM, https://www.npmjs.com/search?q=memorable+password.
+
---
+++ index.js
@@ -0,0 +1,41 @@
+var fs = require("fs");
+
+var readDict = function(fname, minlength) {
+	minlength = minlength || 4;
+	var txt = fs.readFileSync(fname) + "";
+	// console.log(txt);
+	return txt.split("\n").filter((line) => {
+		line = line.trim();
+		if (line[0] == ";")
+			return false;
+		if (line.length < minlength)
+			return false;
+		if (line.length > 8)
+			return false;
+		return true;
+	}).map((line) => {
+		return line.replace(/-|\s/g, "");
+	});
+};
+
+function getRandomIntInclusive(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+String.prototype.ucfirst = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+var generateHappyPass = function() {
+	$positiveWords = readDict("positive-words.txt");
+	var ints = [ getRandomIntInclusive(1, $positiveWords.length), getRandomIntInclusive(1, $positiveWords.length) , getRandomIntInclusive(1, $positiveWords.length) ];
+	return $positiveWords[ints[0]].ucfirst() + $positiveWords[ints[1]].ucfirst() + getRandomIntInclusive(10, 999);
+};
+
+for(var x = 0; x < 99; x++)
+	console.log(generateHappyPass());
+
+module.generate = generateHappyPass;
+// console.log($s.length);
